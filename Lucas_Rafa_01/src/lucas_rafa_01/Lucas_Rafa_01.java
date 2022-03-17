220cdd17f6a578c1adb194c4de67db4a11ecb90a
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-03-17 09:28:51 -0300

    Adicionando o arquivo feito em aula.

---
+++ Lucas_Rafa_01/src/lucas_rafa_01/Lucas_Rafa_01.java
@@ -0,0 +1,62 @@
+package lucas_rafa_01;
+import java.util.Scanner;
+
+public class Lucas_Rafa_01 {
+
+    public static void main(String[] args) {
+        String numero[] = {[],[]};
+        System.out.print("Digite um número entre 1 e 999. Por favor, digitar '99' como 099: ");
+        Scanner entrada = new Scanner(System.in);
+        String numeroEntrada = entrada.nextLine();    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        String centena[] = {"Cem","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos",""};
+        String dezena[] = {"Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa",""};
+        String unidade[] = {"um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",""};
+        String outros[] = {"dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesste", "dezoito", "dezenove"};
+        String c,d,u;
+        System.out.print("Digite um número entre 1 e 999. Por favor, digitar '99' como 099: ");
+        Scanner entrada = new Scanner(System.in);
+        String numero = entrada.nextLine();
+        switch(numero.charAt(2)){
+            case("1"):
+                u = unidade[0];
+                break;
+            case("1"):
+                u = unidade[0];
+                break;
+            case("1"):
+                u = unidade[0];
+                break;
+            case("1"):
+                u = unidade[0];
+                break;
+            case("1"):
+                u = unidade[0];
+                break;
+        }*/
+    }
+    
+}
