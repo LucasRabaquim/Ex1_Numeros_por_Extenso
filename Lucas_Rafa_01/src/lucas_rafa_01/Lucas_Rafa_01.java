fd66dcf09b01876654ead3d3fc4fe3a07d000e20
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-03-17 10:39:06 -0300

    Nova versão

--- Lucas_Rafa_01/src/lucas_rafa_01/Lucas_Rafa_01.java
+++ Lucas_Rafa_01/src/lucas_rafa_01/Lucas_Rafa_01.java
@@ -1,41 +1,43 @@
-package lucas_rafa_01;
+ppackage lucas_rafa_01;
 import java.util.Scanner;
 
 public class Lucas_Rafa_01 {
 
     public static void main(String[] args) {
-        String numero[] = {[],[]};
+        String centena[] = {"","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos"};
+        String dezena[] = {"","Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"};
+        String unidade[] = {"","um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"};
+        String outros[] = {"dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesste", "dezoito", "dezenove","Cem"};
         System.out.print("Digite um número entre 1 e 999. Por favor, digitar '99' como 099: ");
         Scanner entrada = new Scanner(System.in);
-        String numeroEntrada = entrada.nextLine();    
+        String numeroEntrada = entrada.nextLine();
+        char c = numeroEntrada.charAt(0);
+        char d = numeroEntrada.charAt(1);
+        char u = numeroEntrada.charAt(2);
+       String uni[0][2] = {
+        //0   {"","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos"};
+         //1  {"","Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"};
+       }
+        System.out.print(c+""+d+""+u);
+      
         
         
         
         
         
         
+     
+        //numero[0] -> centena
+        //numero[1][3][4] -> centena = Cento, Dezena = Trinta, unidade = quatro,
         
         
         
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
         /*
-        String centena[] = {"Cem","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos",""};
-        String dezena[] = {"Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa",""};
-        String unidade[] = {"um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove",""};
-        String outros[] = {"dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesste", "dezoito", "dezenove"};
+        
         String c,d,u;
         System.out.print("Digite um número entre 1 e 999. Por favor, digitar '99' como 099: ");
         Scanner entrada = new Scanner(System.in);
