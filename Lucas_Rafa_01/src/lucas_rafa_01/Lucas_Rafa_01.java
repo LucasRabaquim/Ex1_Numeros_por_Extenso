e317c534541dbb69f92f8ece0bf8ad414129fac8
Lucas Daniel da Costa Rabaquim <98958822+LucasRabaquim@users.noreply.github.com>
2022-03-18 12:11:27 -0300

    Dia 18.03

--- Lucas_Rafa_01/src/lucas_rafa_01/Lucas_Rafa_01.java
+++ Lucas_Rafa_01/src/lucas_rafa_01/Lucas_Rafa_01.java
@@ -1,24 +1,25 @@
-ppackage lucas_rafa_01;
+package lucas_rafa_01;
 import java.util.Scanner;
 
 public class Lucas_Rafa_01 {
 
     public static void main(String[] args) {
-        String centena[] = {"","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos"};
-        String dezena[] = {"","Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"};
-        String unidade[] = {"","um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"};
+        String numero[][] = {
+            {"","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos"},
+            {"","","Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"},
+            {"","um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"}
+        };
         String outros[] = {"dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezesste", "dezoito", "dezenove","Cem"};
         System.out.print("Digite um número entre 1 e 999. Por favor, digitar '99' como 099: ");
         Scanner entrada = new Scanner(System.in);
         String numeroEntrada = entrada.nextLine();
-        char c = numeroEntrada.charAt(0);
-        char d = numeroEntrada.charAt(1);
-        char u = numeroEntrada.charAt(2);
-       String uni[0][2] = {
-        //0   {"","Cento","Duzentos","Trezentos","Quatrocentos","Quinhentos","Seiscentos","Setecentos","Oitocententos","Novecentos"};
-         //1  {"","Vinte", "Trinta", "Quarenta", "Cinquenta", "Sessenta", "Setenta", "Oitenta", "Noventa"};
-       }
-        System.out.print(c+""+d+""+u);
+        
+        int c = (int)(numeroEntrada.charAt(0));
+        int d = (int)(numeroEntrada.charAt(1));
+        int u = (int)(numeroEntrada.charAt(2));
+        
+
+        System.out.print(numero[0][c] + " e " + numero[1][d] + " e " + numero[2][u]);
       
         
         
@@ -27,15 +28,7 @@ public class Lucas_Rafa_01 {
         
         
      
-        //numero[0] -> centena
-        //numero[1][3][4] -> centena = Cento, Dezena = Trinta, unidade = quatro,
-        
-        
-        
-        
-        
-        
-
+       
         /*
         
         String c,d,u;
